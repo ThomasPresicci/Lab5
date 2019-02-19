ab3d0f5d6bb342561201bbd616c53201e7d28ed2
Thomas Presicci <thomaspresicci@gmail.com>
2019-02-19 17:50:53 -0600

    Filled out taks and time estimates

--- Lab5/src/Todo.java
+++ Lab5/src/Todo.java
@@ -1,18 +1,27 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
+Lab Section_________: 12
+Last Modified_______: 2019-02-19
 OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+OU EMAIL____________: thomaspresicci@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Implement parseAndExecute method in Calculator.java
+2. Implement execute method in Calculator.java
+3. Implement calculateTwoTokens method in Calculator.java
+4. Implement calculateThreeTokens method in Calculator.java
+5. Implement and run tests on Calculator.java
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 5
+2. 15
+3. 20
+4. 20
+5. 25
 
 Actual Time (minutes):
 1. XXX
 2. XXX
+3. 
+4. 
+5. 
 */}
